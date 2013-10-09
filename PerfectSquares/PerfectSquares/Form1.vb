214dc03f8f67e6612e7dad6d7e3cb01fd8ee8c8f
Christiana Lim <limxx277@umn.edu>
2013-10-09 15:14:55 -0500

    commit

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -3,6 +3,15 @@
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
 
         lstResult.Items.Clear()
+
+        Dim i As Integer
+
+        i = 1
+        Do While (i <= 10)
+            lstResult.Items.Add("Number is " & i ^ 2)
+            i = i + 1
+        Loop
+
         lstResult.Items.Add("Not implemented yet")
 
     End Sub
